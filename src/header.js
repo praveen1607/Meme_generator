e6d68bb0e6fb94611db592d8ed8ad04046986cee
praveen <praveenvamsivasamsetti1607@gmail.com>
2021-02-04 22:05:19 +0530

    final2

---
+++ src/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function header(){
+    return(
+        <header>
+            <img
+            src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+            alt="problem"
+            />
+            <p>Meme generator</p>
+        </header>
+    )
+}
+export default header
